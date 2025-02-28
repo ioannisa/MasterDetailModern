0d133e1aac82d42337c7a5f41d2f364b486dd017
Ioannis Anyfantakis <ianyfantakis@deloitte.gr>
2025-02-28 16:56:56 +0200

    Fixed Dtp to Dto typo

--- app/src/main/java/eu/anifantakis/project/library/masterdetailmodern/movies/data/datasource/RemoteMoviesDataSourceImpl.kt
+++ app/src/main/java/eu/anifantakis/project/library/masterdetailmodern/movies/data/datasource/RemoteMoviesDataSourceImpl.kt
@@ -4,7 +4,7 @@ import eu.anifantakis.project.library.masterdetailmodern.core.data.networking.Mo
 import eu.anifantakis.project.library.masterdetailmodern.core.domain.util.DataError
 import eu.anifantakis.project.library.masterdetailmodern.core.domain.util.DataResult
 import eu.anifantakis.project.library.masterdetailmodern.core.domain.util.map
-import eu.anifantakis.project.library.masterdetailmodern.movies.data.dto.MoviesDtp
+import eu.anifantakis.project.library.masterdetailmodern.movies.data.dto.MoviesDto
 import eu.anifantakis.project.library.masterdetailmodern.movies.data.mappers.toMovie
 import eu.anifantakis.project.library.masterdetailmodern.movies.domain.Movie
 import eu.anifantakis.project.library.masterdetailmodern.movies.domain.datasource.RemoteMoviesDataSource
@@ -13,8 +13,14 @@ class RemoteMoviesDataSourceImpl(
     private val httpClient: MoviesHttpClient
 ) : RemoteMoviesDataSource {
     override suspend fun getMovies(): DataResult<List<Movie>, DataError.Network> {
-        return httpClient.get<MoviesDtp>(
-            route = "/3/movie/top_rated"
+        return httpClient.get<MoviesDto>(
+            route = "/3/movie/top_rated",
+
+            // example parameters
+//            queryParameters = mapOf(
+//                "language" to "en-US",
+//                "page" to 1
+//            )
         ).map { moviesDtp ->
             moviesDtp.results.map { movieDto ->
                 movieDto.toMovie()
--- app/src/main/java/eu/anifantakis/project/library/masterdetailmodern/movies/data/dto/MovieDto.kt
+++ app/src/main/java/eu/anifantakis/project/library/masterdetailmodern/movies/data/dto/MovieDto.kt
@@ -4,7 +4,7 @@ import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class MoviesDtp(
+data class MoviesDto(
     val results: List<MovieDto>
 )
 
